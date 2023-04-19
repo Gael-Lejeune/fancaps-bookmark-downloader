9bb4f41dbfc99410592622f0a07222ca6605d593
gael-lejeune <49199682+Gael-Lejeune@users.noreply.github.com>
2023-04-19 10:10:33 +0200

    Added error support and renaming when downloading

--- main.py
+++ main.py
@@ -26,21 +26,36 @@ def main():
 
     # Download the pictures
     print("Downloading the pictures into {}..".format(output_folder))
-    subprocess.call(["python", "dl.py", type_of_media, "list.txt", output_folder])
+    subprocess.call(["python", "dl.py", type_of_media, folder_name, "list.txt", output_folder])
 
     # Print stats about the download like : 
     # Number of pictures downloaded : 10
     # Number of pictures in the folder : 10
+    # Number of pictures for each movie : 
+    # - Mononke : 5
+    # - Kiki : 10
     # Size of the output folder : 2.5 MB
     number_of_pictures_downloaded = sum(1 for line in open("list.txt"))
     number_of_pictures_in_folder = len([name for name in os.listdir(output_folder) if os.path.isfile(os.path.join(output_folder, name))])
+    number_of_pictures_for_each_movie = {}
+    size_of_picture_for_each_movie = {}
+    for file in os.listdir(output_folder):
+        if file.split("_")[0] in number_of_pictures_for_each_movie:
+            number_of_pictures_for_each_movie[file.split("_")[0]] += 1
+            size_of_picture_for_each_movie[file.split("_")[0]] += os.path.getsize(os.path.join(output_folder, file))
+        else:
+            number_of_pictures_for_each_movie[file.split("_")[0]] = 1
+            size_of_picture_for_each_movie[file.split("_")[0]] = os.path.getsize(os.path.join(output_folder, file))
     size_of_output_folder = 0
     for file in os.listdir(output_folder):
         size_of_output_folder += os.path.getsize(os.path.join(output_folder, file))
     size_of_output_folder = size_of_output_folder / 1000000
-    print("Number of pictures downloaded : {}".format(number_of_pictures_downloaded))
-    print("Number of pictures in the folder : {}".format(number_of_pictures_in_folder))
+    print("\n\033[92mNumber of pictures downloaded : {}\033[0m".format(number_of_pictures_downloaded))
     print("Size of the output folder : {} MB".format(round(size_of_output_folder, 3)))
+    print("Number of pictures in the folder : {}".format(number_of_pictures_in_folder))
+    for movie in number_of_pictures_for_each_movie:
+        print("\t- {} : {} ({} MB)".format(movie, number_of_pictures_for_each_movie[movie], round(size_of_picture_for_each_movie[movie] / 1000000, 3)))
+
 
 
 if __name__ == "__main__":
